3229c3efdc929191dafa5b5360e57cef8011b6ba
rozamurzabekova <7893770@mail.ru>
2016-02-16 14:40:53 +0600

    basic.operation

--- calc1/Form1.cs
+++ calc1/Form1.cs
@@ -16,5 +16,16 @@ namespace calc1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstValue = textBox1.Text;
+            string secondValue = textBox2.Text;
+            int first = Convert.ToInt32(firstValue);
+            int second = Convert.ToInt32(secondValue);
+            int third = first + second;
+            rez.Text = third.ToString();
+
+        }
     }
 }
