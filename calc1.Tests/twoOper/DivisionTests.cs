bbaef2f7620d901c1197fc6c79e409c53f9d335d
a <sd>
2016-04-26 14:42:20 +0600

    Tests

---
+++ calc1.Tests/twoOper/DivisionTests.cs
@@ -0,0 +1,17 @@
+﻿using calc1.twoOper;
+using NUnit.Framework;
+
+namespace calc1.Tests.twoOper
+{
+    [TestFixture]
+    public class DivisionTests
+    {
+        [Test]
+        public void BasikTest()
+        {
+            ICalculate calculate = new Division();
+            double result = calculate.Calculate(1, 1);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ calc1.Tests/twoOper/addTests.cs
@@ -0,0 +1,17 @@
+﻿using calc1.twoOper;
+using NUnit.Framework;
+
+namespace calc1.Tests.twoOper
+{
+[TestFixture]
+    public class addTests
+    {
+    [Test]
+    public void BasikTest()
+    {
+        ICalculate calculate=new Add();
+        double result = calculate.Calculate(1, 1);
+        Assert.AreEqual(2, result);
+    }
+    }
+}
